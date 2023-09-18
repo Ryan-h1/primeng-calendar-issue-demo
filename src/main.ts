ed13158fe98a107d586b7cd2b3829cad870c86c7
Ryan-h1 <ryan.hecht123@gmail.com>
2023-09-18 13:23:12 -0400

    Added an import for zone.js in the main.ts final as a hack to allow the project to run on StackBlitz

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import 'zone.js'; // Included with Angular CLI, but a hack to run on StackBlitz
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
